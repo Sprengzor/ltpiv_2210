e68a505c8f612992104d209df8899141bdacc172
Sprengzor <otto.spreng@sou.unifeob.edu.br>
2019-10-27 20:09:46 -0300

    CodeQuinta as redux_playground && desvendando_red

---
+++ desvendando_redux/src/Components/Video/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+// import { Container } from './styles';
+
+const Video = ({ activeModule, activeLesson}) => (
+  <div>
+    <strong> Módulo {activeModule.title}</strong>
+    <span>Aula {activeLesson.title}</span>
+  </div>
+  );
+
+const mapStateToProps = state => ({
+  activeLesson: state.course.activeLesson,
+  activeModule: state.course.activeModule,
+});
+
+export default connect(mapStateToProps)(Video);
---
+++ redux_playground/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { Provider } from "react-redux";
+
+import store from './store';
+
+import TodoList from './TodoList';
+import Counter from './Counter';
+
+export default class App extends Component {
+  render() {
+    return (
+      <Provider store={store} >
+        <TodoList />
+        <Counter />
+      </Provider>
+    );
+  }
+}
